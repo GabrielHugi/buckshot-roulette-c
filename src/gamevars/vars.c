dc62883b7c4c3dce5124fe6ae4a456eafb571c66
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-06-17 14:35:58 -0300

    save

--- src/gamevars/vars.c
+++ src/gamevars/vars.c
@@ -1,6 +1,4 @@
 #include "vars.h"
 
 struct character player;
-char playerInv[8][15];
 struct character dealer;
-char dealerInv[8][15];
--- src/gamevars/vars.h
+++ src/gamevars/vars.h
@@ -1,13 +1,25 @@
 // player "object" (but let's agree to not call it object because object oriented programming is for losers)
 struct character {
     int hp;
+    char inv[8][15];
 };
 
-// the lifes player has left
+struct shotgun {
+    // 0 = nothing, 1 = blank, 2 = live
+    int loadOrder[8];
+}
+
+struct item {
+    /*
+    0 - 
+    1 - 
+    2 -
+    */
+    int effect;
+    
+}
+
+// the player
 extern struct character player;
-// items player has
-extern char playerInv[8][15];
-// the lifes dealer has left
+// the dealer
 extern struct character dealer;
-// items dealer has
-extern char dealerInv[8][15];
